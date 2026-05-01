5d73c1521ad3cef996a8d1248f673a9094284cab
Xuan Hung <maihung200401@gmail.com>
2026-05-01 17:14:05 +0700

    p

--- proxy.ts
+++ proxy.ts
@@ -1,24 +1,47 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-
+ 
+// This function can be marked `async` if using `await` inside
 export function proxy(request: NextRequest) {
+
+  const accessToken = request.cookies.get('accessToken')?.value
   const refreshToken = request.cookies.get('refreshToken')?.value
 
-  const pathname = request.nextUrl.pathname
+  
 
-  if (pathname.startsWith('/signin') || pathname.startsWith('/signup')) {
-    return NextResponse.next()
-  }
+  // CHỈ redirect khi MẤT CẢ HAI token
+  // Nếu mất Access nhưng còn Refresh -> Cho qua để Client tự Refresh
+  if (accessToken) {
+  return NextResponse.next();
+}
 
-  if (!refreshToken) {
+if (!accessToken && refreshToken) {
+  return NextResponse.next();
+}
+
+  // Chỉ khi mất cả hai mới đá ra signin
+  if (!accessToken && !refreshToken) {
     return NextResponse.redirect(new URL('/signin', request.url))
   }
 
   return NextResponse.next()
 }
 
+ 
+// Alternatively, you can use a default export:
+// export default function proxy(request: NextRequest) { ... }
+ 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:png|jpg|jpeg|svg|gif|webp|css|js|map)$).*)',
+    /*
+      Áp dụng cho TẤT CẢ route
+      NGOẠI TRỪ:
+      - _next (static, image)
+      - favicon
+      - file tĩnh: png, jpg, jpeg, svg, gif, webp
+      - css, js, map
+      - signin, signup (public page)
+    */
+    '/((?!_next/static|_next/image|favicon.ico|signin|signup|.*\\.(?:png|jpg|jpeg|svg|gif|webp|css|js|map)$).*)',
   ],
 }
