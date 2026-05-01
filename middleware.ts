61ff7b0a92efd25801de8df8cc07db7a4bdfd008
Xuan Hung <maihung200401@gmail.com>
2026-05-01 22:39:59 +0700

    change name middleware

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 // PHẢI đổi tên từ 'proxy' thành 'middleware'
-export function proxy(request: NextRequest) {
+export function middleware(request: NextRequest) {
   const accessToken = request.cookies.get('accessToken')?.value
   const refreshToken = request.cookies.get('refreshToken')?.value
   const { pathname } = request.nextUrl
